02a175528edb847e75f97676b32ac6d2007f7f02
Tyler Mercer <acct@tylermercer.net>
2025-01-22 20:48:48 -0700

    Add shared lib folder
    Copied from my other projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 // @ts-check
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import remarkFootnote from 'remark-footnotes';
+import remarkEmdash from './lib/plugins/remark/emdash.js';
+import rawFonts from './lib/plugins/vite/rawFonts.js';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  markdown: {
+    remarkPlugins: [
+      // @ts-ignore -- doesn't like remarkFootnote
+      [remarkFootnote, {
+        inlineNotes: true
+      }],
+      remarkEmdash],
+  },
+  vite: {
+    plugins: [rawFonts(['.woff'])],
+    ssr: {
+      external: [
+        'astro/container',
+        'crypto',
+        'fs',
+        'path',
+        'sharp',
+        'esbuild',
+      ].flatMap(id => [id, `node:${id}`]),
+    },
+  },
 });
